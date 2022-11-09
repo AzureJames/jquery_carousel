9fbd76be2eecfc80b52f8f543a597809856b4267
AzureJames <azurethegreat@gmail.com>
2022-11-09 14:54:50 -0700

    works well

--- main.js
+++ main.js
@@ -11,48 +11,39 @@ $(document).ready(function() {
     let carouselWidth = 600;
     let amtOfImgs = 4;
 
-    $(".left-button").click( (e) => {
-        currentLeft = $(".carousel-container").scrollLeft();
-        if (currentLeft % carouselWidth === 0) {
-            $(".carousel-container").animate({
-                scrollLeft: currentLeft - carouselWidth,
-            }, 400);
-            if($(".carousel-container").scrollLeft() == 0){ $(".left-button").addClass("no-display"); }
-            else{$(".left-button").show(); $(".right-button").removeClass("no-display");}
-            $(e.target).css("background-color", "lightcyan");
+        function scrollToLeft() {
+            currentLeft = $(".carousel-container").scrollLeft();
+            if (currentLeft % carouselWidth === 0) {
+                $(".carousel-container").animate({
+                    scrollLeft: currentLeft - carouselWidth,
+                }, 400);
+                if($(".carousel-container").scrollLeft() == 0){ $(".left-button").addClass("no-display"); }
+                else{$(".left-button").show(); $(".right-button").removeClass("no-display");}
+                $(".left-button").css("background-color", "lightcyan");
+            }
+        }
+
+        function scrollToRight() {
+            currentLeft = $(".carousel-container").scrollLeft();
+            if (currentLeft % carouselWidth === 0) {
+                $(".carousel-container").animate({
+                    scrollLeft: currentLeft + carouselWidth,
+                }, 400);
+                if($(".carousel-container").scrollLeft() == (amtOfImgs-1)*carouselWidth){ $(".right-button").addClass("no-display"); }
+                else{$(".right-button").show(); $(".left-button").removeClass("no-display")}
+                $(".right-button").css("background-color", "lightcyan");
+            }
+        }
+
+
+    $(".left-button").on("click", scrollToLeft);
+    $(".right-button").on("click", scrollToRight);
+    $(document).keydown(function(e) {
+        if(e.keyCode == 37){
+            scrollToLeft();
+        }
+        if(e.keyCode == 39){
+            scrollToRight();
         }
     });
-    $(".right-button").click( (e) => {
-        currentLeft = $(".carousel-container").scrollLeft();
-        if (currentLeft % carouselWidth === 0) {
-            $(".carousel-container").animate({
-                scrollLeft: currentLeft + carouselWidth,
-            }, 400);
-            if($(".carousel-container").scrollLeft() == (amtOfImgs-1)*carouselWidth){ $(".right-button").addClass("no-display"); }
-            else{$(".right-button").show(); $(".left-button").removeClass("no-display")}
-            $(e.target).css("background-color", "lightcyan");
-        }
-    });
-
-
-// toggleClass
-// scrollLeft carousel
-
-
-// .text()  get/set 
-// .width
-
-
-// .toggle (display/hide)  
-// .fadein .fadeout
-// .animate
-
-
-// .submit
-// .mouseover
-// .hover .click()  
-// $( "p" ).blur();
-
-// some sorta custom function 
-
 })
