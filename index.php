244f1d0446ffd1759501a788463c6f6519d1060a
AzureJames <azurethegreat@gmail.com>
2022-11-09 12:31:38 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jquery Lab</title>
+    <script
+			  src="https://code.jquery.com/jquery-3.6.1.min.js"
+			  integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
+			  crossorigin="anonymous"></script>
+    <script src="main.js" defer></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        
+            <h1>Jquery Carousel</h1>
+        
+    </header>
+    <div class="container">
+
+        <div class="carousel">
+            <button class="left-button"><--</button>
+            <button class="right-button">--></button>
+            <div class="carousel-container">
+                <div class="carousel-section">
+                    <img src="img/f4-60c-n.jpg" alt="cold glidewax"></img>
+                    <div class="text-container-car">
+                        <h2>F4-60C-N Glidewax Cold 60g w/cork</h2>
+                        <p>Hard F4 glide wax for skis and snowboards. Temperature range -4 and colder.</p>
+                        <p>The F4-60C-N has cork on the back of the box. Rub or dab the glide wax on the base. Polish the wax and the base with the cork.</p>
+                    </div>
+                </div>
+                <div class="carousel-section">
+                    <img src="img/psp-3.jpg" alt="Polar Powder"></img>
+                    <div class="text-container-car">
+                        <h2>PS Polar Powder, -14°C/-32°C, 30g</h2>
+                        <p>PSP is a special powder used to increase the durability of other waxes and reduce the wear on the wax layers. In conditions when the base is subject to extensive wear from e.g. new artificial snow, the relevant wax for the conditions is applied and ironed in first. While the wax is still liquid/warm, spread the PSP powder over the ski base. Then warm the powder into the layer of wax. Allow to cool then scrape off and brush away any excess wax. Recommended waxing iron temp.: 150°C. Replaces CH3X.</p>
+                    </div>
+                </div>
+                <div class="carousel-section">
+                    <img src="img/v0030.jpg" alt="Blue hardwax"></img>
+                    <div class="text-container-car">
+                        <h2>V30 Blue Hardwax -2/-10C, 43g</h2>
+                        <p>Blue grip wax in the classic V-series. Provides great grip for ski trips, training and racing. Apply in thin layers and smooth with a cork.</p>
+                        <p>The V-series is designed for both racing and recreational skiing. The quality is very high due to carefully-selected, sophisticated wax qualities, synthetic rubbers and oils that meet strict health requirements.</p>
+                    </div>
+                </div>
+                <div class="carousel-section">
+                    <img src="img/v0045.jpg" alt="Violet Special Hardwax"></img>
+                    <div class="text-container-car">
+                        <h2>V45 Violet Special Hardwax 0/-3C, 43g</h2>
+                        <p>Violet Special grip wax in the classic V-series. Provides great grip for ski trips, training and racing. Apply in thin layers and smooth with a cork.</p>
+                        <p>New ,falling snow from 0°C to -3°C. Old/transformed snow from -2°C to -6°C. An important wax for racing and recreational skiing. When Blue Extra starts slipping, Violet Special will often be the best wax to use.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <p>2022 Azure James. For educational purposes only.</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+    if($(".carousel-container").scrollLeft() == 0){ $(".left-button").addClass("no-display"); }
+
+    $(".carousel button").blur( (e) => {
+        let bg = $(e.target).css("background-color"); 
+        bg = "lightcyan" ? $(e.target).css("background-color", "lightblue") : $(e.target).css("background-color", "lightcyan");
+    } );
+    
+    
+    let currentLeft;
+    let carouselWidth = 600;
+    let amtOfImgs = 4;
+
+    $(".left-button").click( (e) => {
+        currentLeft = $(".carousel-container").scrollLeft();
+        if (currentLeft % carouselWidth === 0) {
+            $(".carousel-container").animate({
+                scrollLeft: currentLeft - carouselWidth,
+            }, 400);
+            if($(".carousel-container").scrollLeft() == 0){ $(".left-button").addClass("no-display"); }
+            else{$(".left-button").show(); $(".right-button").removeClass("no-display");}
+            $(e.target).css("background-color", "lightcyan");
+        }
+    });
+    $(".right-button").click( (e) => {
+        currentLeft = $(".carousel-container").scrollLeft();
+        if (currentLeft % carouselWidth === 0) {
+            $(".carousel-container").animate({
+                scrollLeft: currentLeft + carouselWidth,
+            }, 400);
+            if($(".carousel-container").scrollLeft() == (amtOfImgs-1)*carouselWidth){ $(".right-button").addClass("no-display"); }
+            else{$(".right-button").show(); $(".left-button").removeClass("no-display")}
+            $(e.target).css("background-color", "lightcyan");
+        }
+    });
+
+// toggleClass
+// scrollLeft carousel
+
+
+// .text()  get/set 
+// .width
+
+
+// .toggle (display/hide)  
+// .fadein .fadeout
+// .animate
+
+
+// .submit
+// .mouseover
+// .hover .click()  
+// $( "p" ).blur();
+
+// some sorta custom function 
+
+})
